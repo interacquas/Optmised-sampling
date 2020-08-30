20ebedfd8a32c00d79d1e1e39618aa2af513e669
interacquas <marco.dantraccoli@biologia.unipi.it>
2020-08-30 18:04:12 +0200

    #

--- Fare la mappa di Ignoranza.R
+++ Fare la mappa di Ignoranza.R
@@ -1,5 +1,9 @@
+# Creare la mappa di ignoranza delle Toscana
+
+
 library(raster)
 library(sp)
+library(ignobioR)
 
 italia <- raster::getData('GADM', country='ITA', level=1) # creo lo shapefile dell'Italia
 plot(italia)
@@ -8,4 +12,5 @@ toscana <- toscana[toscana$NAME_1 == "Toscana",] # subsetto la Toscana
 plot(toscana)
 
 
-ignorance_map(wiki_final, )
+map <- ignorance_map(wiki_final, site=toscana, year_study=2020, excl_areas = NULL, CRS.new = 3035, tau =20, cellsize= 1000)
+
