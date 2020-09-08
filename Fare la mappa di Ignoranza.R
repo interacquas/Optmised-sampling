fb71e088aabd01a9f8c59c32787fe6b4fe212d72
otilgreint <antonelli.gpp@gmail.com>
2020-09-08 10:15:25 +0200

    #

--- Fare la mappa di Ignoranza.R
+++ Fare la mappa di Ignoranza.R
@@ -12,9 +12,9 @@ plot(italia_prov)
 toscana <- italia_reg[italia_reg$NAME_1 == "Toscana",] # subsetto la Toscana
 plot(toscana)
 
-pisalucca<- italia_prov[italia_prov$NAME_2 == "Pisa" | italia_prov$NAME_2 == "Lucca",] # subsetto la provincia
-plot(pisalucca)
+livogross<- italia_prov[italia_prov$NAME_2 == "Livorno" | italia_prov$NAME_2 == "Grosseto",] # subsetto la provincia
+plot(livogross)
 
 wiki_final <- wiki_final[-c(1)] #tolgo la prima colonna a wiki_final
-map <- ignorance_map(wiki_final, site=toscana, year_study=2020, excl_areas = NULL, CRS.new = 3035, tau =20, cellsize= 1000) 
+map <- ignorance_map(wiki_final, site=toscana, year_study=2020, excl_areas = NULL, CRS.new = 3035, tau =20, cellsize= 5000) 
 
