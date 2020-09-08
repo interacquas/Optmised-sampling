2adee5a7bf20ebb7a7e0b64674be9ecc07ac631c
otilgreint <antonelli.gpp@gmail.com>
2020-09-08 21:49:07 +0200

    #

--- virtual_list.R
+++ virtual_list.R
@@ -9,6 +9,7 @@ data(unsuitablezone)  #layer mare
 
 wiki_final <- read_csv("wiki_final.csv")
 wiki_final <- wiki_final[-c(1)]
+wiki_final <- as.data.frame(wiki_final)
 head(wiki_final)
 
 area_studio <- readOGR(dsn = 'CELLA TARGET (materiale extra)', layer = 'Cella per campionamento_WGS84')
@@ -20,5 +21,4 @@ plot(italia_prov)
 livorno <- italia_prov[italia_prov$NAME_2 == 'Livorno',]
 plot(livorno)
 
-wiki_final <- as.data.frame(wiki_final)
 vfl <- virtual_list(data_flor=wiki_final,  site=livorno, excl_areas = unsuitablezone, tau=20)
