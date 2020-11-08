a7adbe533339f87c8b486f9d0342e7922897f42a
otilgreint <antonelli.gpp@gmail.com>
2020-11-08 12:46:08 +0100

    #

--- lavoro su ndvi/lavoro su ndvi.r
+++ lavoro su ndvi/lavoro su ndvi.r
@@ -6,3 +6,5 @@ library(maptools)
 buffer_plot <- raster("C:/Users/Giuseppe Antonelli/Desktop/tirocinio/Optmised-sampling/lavoro su ndvi/NDVI buffer plot.tif")
 zone_buffer <- raster("C:/Users/Giuseppe Antonelli/Desktop/tirocinio/Optmised-sampling/lavoro su ndvi/NDVI zone campionate buffer.tif")
 zone <- raster("C:/Users/Giuseppe Antonelli/Desktop/tirocinio/Optmised-sampling/lavoro su ndvi/NDVI zone campionate.tif")
+
+plot(buffer_plot)
