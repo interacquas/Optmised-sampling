eaf75b7375415fa335e2968ca81f09fed8c84bc3
interacquas <marco.dantraccoli@biologia.unipi.it>
2021-01-06 12:44:37 +0100

    #

--- Analisi- parte 1.R
+++ Analisi- parte 1.R
@@ -4,6 +4,10 @@ library(readxl)
 library(splitstackshape)
 library(vegan)
 library(ggplot2)
+library(raster)
+library(rgdal)
+library(sp)
+library(maptools)
 
 ##### carico il dataframe specie con nome df
 
@@ -19,8 +23,23 @@ df_coord$X <- as.numeric(df_coord$X)
 df_coord$Y <- as.numeric(df_coord$Y)
 
 
-df_spectra <- read_excel("C:/Users/MARCO/Documents/Tesi laureandi/Giuseppe Antonelli/specie-per-plot_coord.xlsx", sheet = "spectral")
-df_spectra <- as.data.frame(df_spectra) 
+### Dati spettrali
+
+
+zone <- raster("gis zone campionamento/MAPPA NDVI_28m.tif")
+sampling <- readOGR('gis zone campionamento/buffer punti finali campionamento.shp')
+sampling <- readOGR('gis zone campionamento/zone campionate 3035.shp')
+
+
+plot(zone)
+plot(sampling, add=TRUE)
+
+values <- extract(zone, sampling, na.rm =TRUE, fun=median) # faccio il sampling del raster
+
+df_spectra <- as.data.frame(cbind(df_coord$ID, values))
+names(df_spectra) <- c("ID", "ndvi")
+df_spectra$ndvi<- as.numeric(df_spectra$ndvi)
+
 
 #####  creo un dataframe 'presenza-assenza'
 
@@ -137,7 +156,7 @@ legend(8, 40, c("Classic", "SER"), lty=c(1,1),lwd=c(2,2),col =
 
 
 
-############## diretional SAC ################
+############## diretional SAC Bacaro ################
 
 require(Rarefy)
 require(vegan)
@@ -147,7 +166,7 @@ mite.xy <- df_coord[,2:3]
 
 # Spatially-explicit curves can be obtained as follows
 spatialdist <- dist(mite.xy) # to calculate the geographic
-spectraldist <- dist(df_spectra, method = "euclidean")
+spectraldist <- dist(df_spectra, method = "euclidean") # calculating spectral distance
 
 # distance between plots, i.e. the Euclidean distance # between the coordinates of the plots)
 betas <- directionalSAC(comm_matrix, spatialdist) # to calculate directional
@@ -182,12 +201,10 @@ plot(2:12, betas$Beta_Autocor[2:12], xlab="M",
      ylab="Normalized measure of autocorrelation")
 
 
+##### Plot spaziale vs. spettrale con i metodi di Bacaro
 
 
-##### Plot spaziale vs. spettrale
-
-
-plot(seq(1:12), betas[,1], xlim= c(1, 12), ylim=c(0,max(explicit_curve[,1 ])+5),
+plot(seq(1:12), betas[,1], xlim= c(1, 12), ylim=c(0,max(explicit_curve[,1 ])+1),
      xlab="Number of plots", ylab="Species Richness", type="p", pch=10, ann=TRUE,col ="white", main="Rarefaction")
 lines(seq(1:12),betas[,1], lwd=3, col="dark gray")
 axis(side=1, at=c(1:12))
@@ -196,14 +213,156 @@ legend(8, 40, c("Spectral", "Spatial"), lty=c(1,1),lwd=c(2,2),col =
          c("black","darkgrey"), bty="n", text.col = "black", merge = TRUE, cex=1.3, pt.cex=1)
 
 
+##### MODIFICA DELLE FUNZIONI CON LE MASSIME DISTANZE (SPETTRRALI O SPAZIALI), metodo mio
+
+library(rdist)
+
+centroid_pattern_max <- function (z, longlat) {
+  
+  centroid <- function (x) {
+    return(c(X = mean(x$X), Y =mean(x$Y)))
+  }
+  
+  
+  dist_1 <- function (p1, p2) {
+    return(sqrt((p2['X'] - p1['X'])^2+ (p2['Y'] - p1['Y'])^2))
+  }
+  
+  require(sp)
+  if (longlat== TRUE) {  
+    xy <- z[,c(2,3)]
+    colnames(xy) <- c("X", "Y")
+    
+    centroiddata <- SpatialPointsDataFrame(coords = xy, data = z, proj4string = CRS("+init=epsg:4326"))
+    centroiddata <- spTransform(centroiddata,CRS("+init=epsg:3035"))
+    
+    centroiddata <- cbind(as.character(centroiddata$ID), centroiddata@coords)
+    centroiddata <- as.data.frame(centroiddata)
+    
+    centroiddata$X <- as.numeric(as.numeric(as.character(centroiddata$X)))
+    centroiddata$Y <- as.numeric(as.numeric(as.character(centroiddata$Y)))
+    colnames(centroiddata) <- c("ID", "X", "Y")
+    z <- centroiddata
+  }
+  
+  supersequence <- c()
+  for(j in 1:nrow(z)) {
+    df_filter <- c(j)
+    df_nofilter <- (1:nrow(z))[-df_filter]
+    df_centre <- centroid(z[df_filter,])
+    distance_vector <- c()
+    sequence_vector <- as.character(z$ID[df_filter])
+    while(length(df_nofilter>0)) {
+      for (i in 1:length(df_nofilter)) {
+        distance_vector <- c(distance_vector, dist_1(df_centre, z[df_nofilter[i],]))
+        distance_vector <- unlist(distance_vector, use.names=FALSE)
+        
+      }
+      print(paste("Maximum distance",max(distance_vector)))
+      md <- which(distance_vector==max(distance_vector))
+      print(paste("Index of distance_vector:",md))
+      print(paste("Corresponding to pointdata Index",df_nofilter[md]))
+      print(paste("Nearest point:",z$ID[df_nofilter[md]]))
+      sequence_vector <- c(sequence_vector,as.character(z$ID[df_nofilter[md]]))
+      distance_vector <- c()
+      df_filter <- c(df_filter,df_nofilter[md])
+      df_nofilter <- (1:nrow(z))[-df_filter]
+      df_centre <- centroid(z[df_filter,])
+      
+    }
+    supersequence <- c(supersequence,sequence_vector)
+  }
+  seqmatrix <- matrix(supersequence,ncol = nrow(z),byrow = TRUE)
+  seqmatrix <- as.data.frame(seqmatrix)
+  colnames(seqmatrix) <- c("Plot", 1:(nrow(z)-1))
+  return(seqmatrix)
+}
+
+mxp_all_2 <- centroid_pattern_max(df_coord, longlat = TRUE)
+explicit_curve_max <-SCR(df2, t(mxp_all_2))
+
+
+spectral_dist<-vegdist(df_spectra[,2], upper=TRUE, method="euclidean",diag=TRUE)
+yy <- as.matrix(spectral_dist)
+sampling_order <- data.frame()
+for(j in 1:ncol(yy)) {
+  
+  VEC <- yy[,j]
+  
+  
+for(i in 1:(nrow(yy)-1)) { 
+ 
+  SAMP <- max(VEC, na.rm = TRUE)
+  check <- match(SAMP, VEC)
+  sampling_order[j, i] <- check
+  VEC[sampling_order[j, i]] <- NA # questa riga deve rimanere uguale
+ 
+ 
+} 
+  print(j)
+} ### faccio la matrice per la rarefazione spettralmente esplicita!
+
+sampling_order<- cbind(Plot = 1:12, sampling_order)
+sampling_order[] <- lapply(sampling_order, function(x) paste("plot", x, sep=""))
+names(sampling_order) <- c("Plot", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11")
+
+spectral_curve_max <-SCR(df2, t(sampling_order))
+explicit_curve_max <-SCR(df2, t(mxp_all_2))
+
+
+plot(seq(1:12), spectral_curve_max[,1], xlim= c(1, 12), ylim=c(0,max(explicit_curve[,1 ])+1),
+     xlab="Number of plots", ylab="Species Richness", type="p", pch=10, ann=TRUE,col ="white", main="Rarefaction")
+lines(seq(1:12),spectral_curve_max[,1], lwd=3, col="dark gray")
+axis(side=1, at=c(1:12))
+lines(seq(1:12),explicit_curve_max[,1], lwd =3, col ="black")
+legend(8, 40, c("Spatial", "Spectral"), lty=c(1,1),lwd=c(2,2),col =
+         c("black","darkgrey"), bty="n", text.col = "black", merge = TRUE, cex=1.3, pt.cex=1)
+
+
+#  beta diversity Baselga
+
+library(betapart)
+
+
+# get betapart objects
+
+ceram.s.core <- betapart.core(df2[,2:ncol(df2)])
+
+# multiple site measures
+
+ceram.s.multi <- beta.multi(ceram.s.core)
+
+
+# sampling across equal sites
+
+ceram.s.samp <- beta.sample(ceram.s.core, sites=12, samples=100)
+
+# plotting the distributions of components
+
+dist.s <- ceram.s.samp$sampled.values
+
+plot(density(dist.s$beta.SOR), xlim=c(0,0.8), ylim=c(0, 22), xlab='Beta diversity', main='', lwd=3)
+
+lines(density(dist.s$beta.SNE), lty=1, lwd=2)
+
+lines(density(dist.s$beta.SIM), lty=2, lwd=2)
+
+
+# pairwise 
+
+pair.s <- beta.pair(df2[,2:ncol(df2)])
+
+# plotting clusters
+
+dist.s <- ceram.s.samp$sampled.values
+
+plot(hclust(pair.s$beta.sim, method='average'), hang=-1, main='', sub='', xlab='')
+title(xlab=expression(beta[sim]), line=0.3)
+
+plot(hclust(pair.s$beta.sne, method='average'), hang=-1, main='', sub='', xlab='')
+title(xlab=expression(beta[sne]), line=0.3)
 
 
 
-
-
-
-
-
-##### BASELGA
 
 
